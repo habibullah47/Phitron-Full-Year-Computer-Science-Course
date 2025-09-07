93a8885c71ad2eea360c2d743bb9b5e93e875db3
habibullah47 <habib.world.121@gmail.com>
2025-09-07 09:48:17 +0600

    feat(6.6): insert at any position in linked list

---
+++ Semester 2/Course 3 - Basic Data Structure/Module 6 - Singly Linked List Operations/6.6_insert_at_any_pos.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class Node {
+public:
+  int val;
+  Node *next;
+
+  Node(int val) {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+
+void insert_at_any_pos(Node *head, int idx, int val) {
+  Node *newNode = new Node(val);
+
+  Node *tmp = head;
+  for(int i = 0; i < idx - 1; i++){
+    tmp = tmp->next;
+  }
+
+  newNode->next = tmp->next;
+  tmp->next = newNode;
+}
+
+void print_linked_list(Node *head) {
+  Node *tmp = head;
+  while (tmp != NULL) {
+    cout << tmp->val << endl;
+    tmp = tmp->next;
+  }
+}
+
+int main() {
+  Node *head = new Node(10);
+  Node *a = new Node(20);
+  Node *b = new Node(30);
+
+  head->next = a;
+  a->next = b;
+
+  insert_at_any_pos(head,2, 100);
+  insert_at_any_pos(head,2, 50);
+  print_linked_list(head);
+
+  return 0;
+}
