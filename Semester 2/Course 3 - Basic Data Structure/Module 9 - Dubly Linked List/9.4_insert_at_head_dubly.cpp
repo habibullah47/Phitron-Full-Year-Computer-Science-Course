9c87bda5b07fe7e51e61392ac0b8e253b03f82ab
habibullah47 <habib.world.121@gmail.com>
2025-09-11 10:39:13 +0600

    feat(9.1 - .6): dubly linked list insertion

---
+++ Semester 2/Course 3 - Basic Data Structure/Module 9 - Dubly Linked List/9.4_insert_at_head_dubly.cpp
@@ -0,0 +1,60 @@
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *next;
+  Node *prev;
+
+  Node(int val) {
+    this->val = val;
+    this->next = NULL;
+    this->prev = NULL;
+  }
+};
+
+void print_forward(Node *head) {
+  Node *tmp = head;
+
+  while (tmp != NULL) {
+    cout << tmp->val << " ";
+    tmp = tmp->next;
+  }
+  cout << endl;
+}
+void print_backward(Node *tail) {
+  Node *tmp = tail;
+
+  while (tmp != NULL) {
+    cout << tmp->val << " ";
+    tmp = tmp->prev;
+  }
+  cout << endl;
+}
+
+void insert_at_head(Node *&head, int val) {
+  Node *newNode = new Node(val);
+
+  newNode->next = head;
+  // newNode = head->prev;
+  head->prev = newNode;
+  head = newNode;
+}
+int main() {
+  Node *head = new Node(10);
+  Node *a = new Node(20);
+  Node *tail = new Node(30);
+
+  head->next = a;
+  a->prev = head;
+
+  a->next = tail;
+  tail->prev = a;
+
+  insert_at_head(head, 100);
+  print_forward(head);
+  print_backward(tail);
+  return 0;
+}
