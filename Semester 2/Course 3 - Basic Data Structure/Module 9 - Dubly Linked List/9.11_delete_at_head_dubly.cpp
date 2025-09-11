886b3b9d8fe2448b3977a07baacc4880cb782b5b
habibullah47 <habib.world.121@gmail.com>
2025-09-11 11:52:00 +0600

    feat(9.11): delete at head in dubly linked list

---
+++ Semester 2/Course 3 - Basic Data Structure/Module 9 - Dubly Linked List/9.11_delete_at_head_dubly.cpp
@@ -0,0 +1,66 @@
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *next;
+  Node *prev;
+
+  Node(int val) {
+    this->val = val;
+    this->next = NULL;
+    this->prev = NULL;
+  }
+};
+
+void print_forward(Node *head) {
+  Node *tmp = head;
+
+  while (tmp != NULL) {
+    cout << tmp->val << " ";
+    tmp = tmp->next;
+  }
+  cout << endl;
+}
+void print_backward(Node *tail) {
+  Node *tmp = tail;
+
+  while (tmp != NULL) {
+    cout << tmp->val << " ";
+    tmp = tmp->prev;
+  }
+  cout << endl;
+}
+
+void delete_at_head(Node *&head, Node *&tail) {
+  Node *deleteNode = head;
+  head = head->next;
+
+  // corner case
+  if (head == NULL) {
+    tail = NULL;
+    return;
+  }
+  head->prev = NULL;
+  delete deleteNode;
+}
+
+int main() {
+  Node *head = new Node(10);
+  Node *a = new Node(20);
+  Node *tail = new Node(30);
+
+  head->next = a;
+  a->prev = head;
+
+  a->next = tail;
+  tail->prev = a;
+
+  delete_at_head(head, tail);
+  delete_at_head(head, tail);
+  print_forward(head);
+  print_backward(tail);
+  return 0;
+}
