73a7bbf4ca053ecfd55b4d343c135dd55053750e
habibullah47 <habib.world.121@gmail.com>
2025-09-21 09:55:52 +0600

    lecture(13.6): stack input n output by array

---
+++ Semester 2/Course 3 - Basic Data Structure/Week 4 - Stack and Queue/Module 13 - Stack Implementation and STL/13.6_stack_input_n_output.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class myStack {
+public:
+  vector<int> v;
+
+  void push(int val) { v.push_back(val); }
+  void pop() { v.pop_back(); }
+  int top() { return v.back(); }
+  int size() { return v.size(); }
+  bool empty() { return v.empty(); }
+};
+
+int main() {
+  myStack st;
+
+  int n, val;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> val;
+    st.push(val);
+  }
+
+  while (!st.empty()) {
+    cout << st.top() << endl;
+    st.pop();
+  }
+
+  return 0;
+}
