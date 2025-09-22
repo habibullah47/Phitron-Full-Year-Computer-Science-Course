8e14ebd849230dce479b1603fbfa6aadbe34bfd5
habibullah47 <habib.world.121@gmail.com>
2025-09-22 08:27:43 +0600

    lecture(14.3): implement queue using singly

---
+++ Semester 2/Course 3 - Basic Data Structure/Week 4 - Stack and Queue/Module 14 - Queue Implementation and STL/14.3_implement_queue_using_sinlgy.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+class Node {
+public:
+  int val;
+  Node *next;
+
+  Node(int val) {
+    this->val = val;
+    this->next = NULL;
+  }
+};
+
+class myQueue {
+public:
+  Node *head = NULL;
+  Node *tail = NULL;
+
+  int sz = 0;
+  void push(int val) { // O(1)
+    sz++;
+    Node *newnode = new Node(val);
+
+    if (head == NULL) {
+      head = newnode;
+      tail = newnode;
+      return;
+    }
+
+    tail->next = newnode;
+    tail = newnode;
+  }
+  void pop() { // O(1)
+    sz--;
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+    if (head == NULL) {
+      tail = NULL;
+    }
+  }
+  int front() { return head->val; } // O(1)
+
+  int back() { return tail->val; }      // O(1)
+  int size() { return sz; }             // O(1)
+  bool empty() { return head == NULL; } // O(1)
+};
+
+int main() {
+  myQueue q;
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    int val;
+    cin >> val;
+    q.push(val);
+  }
+
+  // cout << q.front() << " " << q.back() << " " << q.size();
+
+  while (!q.empty()) {
+    cout << q.front() << endl;
+    q.pop();
+  }
+  return 0;
+}
