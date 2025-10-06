0b2e9d37a18877c0025646b06b7adfb982086441
habibullah47 <habib.world.121@gmail.com>
2025-10-06 09:09:38 +0600

    module(18.5): input binary tree

---
+++ Semester 2/Course 3 - Basic Data Structure/Week 5 - Binary Tree/Module 18 - Operations on Binary Tree/5_binary_tree_input_implementation.cpp
@@ -0,0 +1,132 @@
+#include <cstddef>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *left;
+  Node *right;
+
+  Node(int val) {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+// Node *input_tree() {
+//   int val;
+//   cin >> val;
+//   Node *root;
+//   if (val == -1)
+//     root = NULL;
+//   else
+//     root = new Node(val);
+
+//   queue<Node *> q;
+//   if (root)
+//     q.push(root);
+
+//   while (!q.empty()) {
+//     // 1. ber kore ana
+//     Node *p = q.front();
+//     q.pop();
+
+//     // 2. oi node ke nie kaj
+//     int l, r;
+//     cin >> l >> r;
+
+//     Node *myLeft, *myRight;
+
+//     if (l == -1)
+//       myLeft = NULL;
+//     else
+//       myLeft = new Node(l);
+//     if (r == -1)
+//       myRight = NULL;
+//     else
+//       myRight = new Node(r);
+
+//     p->left = myLeft;
+//     p->right = myRight;
+
+//     // 3. children push kora
+//     if (p->left)
+//       q.push(p->left);
+//     if (p->right)
+//       q.push(p->right);
+//   }
+//   return root;
+// }
+
+Node *input_tree() {
+  int val;
+  cin >> val;
+  Node *root;
+  if (val == -1) {
+    root = NULL;
+  } else {
+    root = new Node(val);
+  }
+
+  queue<Node *> q;
+
+  if (root)
+    q.push(root);
+
+  while (!q.empty()) {
+    Node *parent = q.front();
+    q.pop();
+
+    int l, r;
+    cin >> l >> r;
+
+    Node *myLeft, *myRight;
+
+    if (l == -1) {
+      myLeft = NULL;
+    } else {
+      myLeft = new Node(l);
+    }
+    if (r == -1) {
+      myRight = NULL;
+    } else {
+      myRight = new Node(r);
+    }
+
+    parent->left = myLeft;
+    parent->right = myRight;
+
+    if (parent->left)
+      q.push(parent->left);
+    if (parent->right)
+      q.push(parent->right);
+  }
+  return root;
+}
+
+void level_order(Node *root) {
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty()) {
+    Node *f = q.front();
+    q.pop();
+
+    cout << f->val << " ";
+
+    if (f->left)
+      q.push(f->left);
+    if (f->right)
+      q.push(f->right);
+  }
+}
+
+int main() {
+  Node *root = input_tree();
+  level_order(root);
+
+  return 0;
+}
