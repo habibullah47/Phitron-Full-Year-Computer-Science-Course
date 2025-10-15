3850805c71ee74a64d9a23670f974b7404d70fad
habibullah47 <habib.world.121@gmail.com>
2025-10-15 11:09:20 +0600

    module(21): convert in BST

---
+++ Semester 2/Course 3 - Basic Data Structure/Week 6 - BST, Heap & Map/Module 21 - Binary Search Tree/10.convert_array_to_bst.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *left;
+  Node *right;
+
+  Node(int val) {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+Node *convert(int a[], int l, int r) {
+  if (l > r)
+    return NULL;
+
+  int mid = (l + r) / 2;
+  Node *root = new Node(a[mid]);
+
+  Node *leftroot = convert(a, l, mid - 1);
+  Node *rightroot = convert(a, mid + 1, r);
+
+  root->left = leftroot;
+  root->right = rightroot;
+
+  return root;
+}
+
+void level_order(Node *root) {
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty()) {
+    Node *f = q.front();
+    q.pop();
+
+    cout << f->val << " ";
+
+    if (f->left)
+      q.push(f->left);
+    if (f->right)
+      q.push(f->right);
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int a[n];
+
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  Node *root = convert(a, 0, n - 1);
+  level_order(root);
+
+  return 0;
+}
