2bd436754cc9d8ec4c9a0241fc39c28162fa3eaf
habibullah47 <habib.world.121@gmail.com>
2025-11-17 11:19:16 +0600

    lecture(3.7): DFS on 2D grid Implementation II

--- Semester 2/Course 4 - Introduction to Algorithms/Week 1 - Graph Traversal Algorithms/Module 3 - DFS (Defth First Search)/7 DFS on 2D grid Implementation II.cpp
+++ Semester 2/Course 4 - Introduction to Algorithms/Week 1 - Graph Traversal Algorithms/Module 3 - DFS (Defth First Search)/7 DFS on 2D grid Implementation II.cpp
@@ -5,21 +5,29 @@ using namespace std;
 char grid[105][105];
 bool visited[105][105];
 vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
 
+bool isValid(int i, int j) {
+  if (i < 0 || i >= n || j < 0 || j >= m) {
+    return false;
+  }
+  return true;
+}
 void dfs(int si, int sj) {
   cout << si << " " << sj << endl;
   visited[si][sj] = true;
 
   for (int i = 0; i < 4; i++) {
-    int ci, cj;
-    ci = si + d[i].first;
-    cj = sj + d[i].second;
-    cout << ci << " " << cj << endl;
+
+    int ci = si + d[i].first;
+    int cj = sj + d[i].second;
+    if (isValid(ci, cj) && !visited[ci][cj]) {
+      dfs(ci, cj);
+    }
   }
 }
 
 int main() {
-  int n, m;
   cin >> n >> m;
 
   for (int i = 0; i < n; i++) {
